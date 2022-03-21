8d9f99498ca39233d100e842cd0dc67dfce69b58
trossignol <thomas.rossignol.pro@gmail.com>
2022-03-21 21:58:06 +0000

    rest

---
+++ quarkus-rest/src/main/java/com/decathlon/trossi/FruitResource.java
@@ -0,0 +1,26 @@
+package com.decathlon.trossi;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/fruits")
+public class FruitResource {
+
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit TEST"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
+    @GET
+    public Set<Fruit> list() {
+        return fruits;
+    }
+}
---
+++ spring-boot-rest/src/main/java/com/example/restservice/FruitResource.java
@@ -0,0 +1,24 @@
+package com.example.restservice;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FruitResource {
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
+    @GetMapping("/fruits")
+    public Set<Fruit> list() {
+        return fruits;
+    }
+}
