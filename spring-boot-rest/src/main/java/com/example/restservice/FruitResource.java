d89a1773ab6258deb8756f5a6f5e058197188943
trossignol <thomas.rossignol.pro@gmail.com>
2022-10-15 16:13:51 +0000

    import fix

--- spring-boot-rest/src/main/java/com/example/restservice/FruitResource.java
+++ spring-boot-rest/src/main/java/com/example/restservice/FruitResource.java
@@ -5,7 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.Set;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
