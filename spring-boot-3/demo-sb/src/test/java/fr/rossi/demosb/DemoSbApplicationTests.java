48a53117280d08526f4d9d9dc69b6a139ee6ce08
Thomas Rossignol <thomas.rossignol@decathlon.com>
2022-12-03 11:06:00 +0100

    spring-boot 3

---
+++ spring-boot-3/demo-sb/src/test/java/fr/rossi/demosb/DemoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rossi.demosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
